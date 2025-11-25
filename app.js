1ca2d0e8a8a53d96c431ff8a892f90ca3cf92726
Seonjun Kim <kimsunjun1221@gmail.com>
2025-11-25 20:22:49 +0900

    feat: 무중단 배포 추가

--- app.js
+++ app.js
@@ -11,6 +11,17 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/pages/post/post.html"));
 });
 
+http://localhost:3000/health
+app.get("/health", (req, res) => {
+  const now = new Date();
+	const hh = String(now.getHours()).padStart(2, '0');
+	const mm = String(now.getMinutes()).padStart(2, '0');
+	const ss = String(now.getSeconds()).padStart(2, '0');
+	const timeString = `${hh}:${mm}:${ss}`;
+
+	res.status(200).send(`OK - ${timeString} \n`);
+})
+
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "public/pages/login/login.html"));
 });
